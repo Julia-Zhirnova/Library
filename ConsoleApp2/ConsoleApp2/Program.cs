07d93b90c26a263a9358ffabb908e3045478045d
”Ivan <ivan.ivanov@gmail.com>
2022-03-09 23:37:57 +0300

    init

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreLib;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite kolichestvo othenok");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] marks = new string[n];
+            //string [] marks1;
+            Console.WriteLine("Vvedite othenki");
+            for (int i = 0; i < n; i++)
+            {
+                marks[i] = Console.ReadLine();
+            }
+            double result = utils.MinAVG(marks);
+            Console.WriteLine("Result: {0}", result);
+            int exit = Convert.ToInt32(Console.ReadLine());
+        }
+    
+    }
+}
---
+++ CoreLib/CoreLib/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib
+{
+    public static class utils
+    {
+        // Вычисление среднего арифметического значения оценки в меньшую сторону 
+        public static double MinAVG(string[] marks)
+        {
+            Console.WriteLine("String length: {0}", marks.Length);
+            int[] mass = new int[marks.Length];
+            double res = 0, sum = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                mass[i] = Convert.ToInt32(marks[i]);
+                Console.WriteLine(mass[i]);
+                sum = sum + mass[i];
+            }
+            res = sum / mass.Length;
+            Console.WriteLine(res);
+            int modres = Convert.ToInt32(res);
+            Console.WriteLine(modres);
+            modres = (int)res;
+            return modres;
+        }
+    }
+}
